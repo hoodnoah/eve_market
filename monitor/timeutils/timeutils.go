0429910893ab54e9929c6137c2691a70ee212aea
Noah Hood <hood.noah@icloud.com>
2024-02-14 13:56:05 -0500

    use semantic type for Years

--- monitor/timeutils/timeutils.go
+++ monitor/timeutils/timeutils.go
@@ -5,27 +5,30 @@ import (
 	"time"
 )
 
+// semantic type alias
+type Year uint
+
 // earliest year with data
-const EarliestYear = 2003
+const earliestYear = Year(2003)
 
 // the prefix for the daily export data file
-const FilePrefix = "market-history"
+const filePrefix = "market-history"
 
 // the suffix for the daily export data file
-const FileExt = ".csv.bz2"
+const fileExt = ".csv.bz2"
 
 // first date with data
-var EarliestDate = time.Date(EarliestYear, 10, 1, 0, 0, 0, 0, time.UTC)
+var earliestDate = time.Date(int(earliestYear), 10, 1, 0, 0, 0, 0, time.UTC)
 
 // enumerates the years from the beginning of daily market data collection (2003)
 // to present
-func EnumerateYearsToPresent(startYear int) []int {
-	currentYear := time.Now().Year()
-	numYears := int(currentYear) - int(startYear) + 1
-	years := make([]int, numYears)
+func EnumerateYearsToPresent(startYear Year) []Year {
+	currentYear := Year(time.Now().Year())
+	numYears := currentYear - earliestYear + 1
+	years := make([]Year, numYears)
 
 	for i := range years {
-		years[i] = int(startYear) + i
+		years[i] = startYear + Year(i)
 	}
 
 	return years
@@ -34,14 +37,14 @@ func EnumerateYearsToPresent(startYear int) []int {
 // creates a slice containing all the days within a given integer year
 // excluding the beginning of 2003, since data recording began on
 // 2003-10-01
-func EnumerateDatesInYear(year int) []time.Time {
-	startDate := time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
-	endDate := time.Date(year+1, time.January, 1, 0, 0, 0, 0, time.UTC)
+func EnumerateDatesInYear(year Year) []time.Time {
+	startDate := time.Date(int(year), time.January, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(int(year+1), time.January, 1, 0, 0, 0, 0, time.UTC)
 
 	var dates []time.Time
 
 	for currentDate := startDate; currentDate.Before(endDate); currentDate = currentDate.AddDate(0, 0, 1) {
-		if currentDate.After(EarliestDate) || currentDate.Equal(EarliestDate) {
+		if currentDate.After(earliestDate) || currentDate.Equal(earliestDate) {
 			dates = append(dates, currentDate)
 		}
 	}
@@ -52,5 +55,5 @@ func EnumerateDatesInYear(year int) []time.Time {
 // generates a url suffix for a given day's data,
 // e.g. for '2003-10-01' -> 2003/market-history-2003-10-01.csv.bz2'
 func GetDayFileURL(date time.Time) string {
-	return fmt.Sprintf("%d/%s-%s%s", date.Year(), FilePrefix, date.Format(time.DateOnly), FileExt)
+	return fmt.Sprintf("%d/%s-%s%s", date.Year(), filePrefix, date.Format(time.DateOnly), fileExt)
 }
