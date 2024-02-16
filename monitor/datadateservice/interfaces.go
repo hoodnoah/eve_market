f2baf8b3a3433d5ca67015aa7670d91dcd2df957
Noah Hood <hood.noah@icloud.com>
2024-02-16 15:21:35 -0500

    replace timeutils w/ datadate service

---
+++ monitor/datadateservice/interfaces.go
@@ -0,0 +1,23 @@
+package datadateservice
+
+import "time"
+
+type Year int
+
+// a single date
+type DataDate struct {
+	Date time.Time
+	Url  string
+}
+
+// a year's worth of dates
+type DataYear struct {
+	Year  Year
+	Dates []DataDate
+}
+
+type IDataDateService interface {
+	EnumerateDataYears(currentDate time.Time) []DataYear
+}
+
+type DataDateService struct{}
