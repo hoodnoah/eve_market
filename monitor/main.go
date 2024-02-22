5726ec4705855de5c7264eea1d2c387b27c4dade
Noah Hood <hood.noah@icloud.com>
2024-02-22 15:17:41 -0500

    simplify dateservice

--- monitor/main.go
+++ monitor/main.go
@@ -1,6 +1,13 @@
 package main
 
-import ()
+import (
+	dateservice "github.com/hoodnoah/eve_market/monitor/datadateservice"
+)
 
 func main() {
+	// setup services
+	dateSvc := dateservice.NewDataDateService(dateservice.GetCurrentDate)
+
+	// get dates
+	dataYears := dateSvc.EnumerateDataYears()
 }
